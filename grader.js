dec2539ce4f088f1aff3f84e46d40010c891415b
Andreea Popovici <andreea.popovici1103@yahoo.com>
2013-07-11 12:44:58 +0000

    sper sa mearga

--- grader.js
+++ grader.js
@@ -26,6 +26,12 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT="http://fast-plateau-3932.herokuapp.com/";
+var rest=require('restler');
+var sys=require('util');
+
+var assertUrlExists=function(u){
+ return u.toString();};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +71,21 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>','url to chek',URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+var htmlFile=program.file;
+if(program.url){
+rest.get(program.url).on('complete',function(result){
+fs.writeFileSync("url.html",result);});
+htmlFile="url.html";
+}    
+var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+var checkJson=checkHtmlFile(result,program.checks);
+var outJson=JSON.stringify(checkJson,null,4);
+console.log(outJson);    
+//exports.checkHtmlFile = checkHtmlFile;
 }
